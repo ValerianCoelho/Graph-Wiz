6cb401af634f87b57fd88ef8dc9ec3b92edfe471
Valerian Coelho <valerian27012004@gmail.com>
2023-06-09 18:52:03 +0530

    Interact Animations Implemented

--- src/Components/Node.js
+++ src/Components/Node.js
@@ -1,9 +1,33 @@
+import React, { useRef, useEffect } from 'react';
+import interact from 'interactjs';
 import './Node.css'
 
-export default function Node({label}) {
+export default function Node({label, ref}) {
+  const elementRef = useRef(null);
+
+  useEffect(() => {
+    const element = elementRef.current;
+    const position = { x: 0, y: 0 }
+
+    interact(element).draggable({
+        listeners: {
+          start (event) {
+            console.log(event.type, event.target)
+          },
+          move (event) {
+            position.x += event.dx
+            position.y += event.dy
+      
+            event.target.style.transform =
+              `translate(${position.x}px, ${position.y}px)`
+          },
+        }
+      })
+  }, [])
+  
   return (
     <>
-      <div className="node">
+      <div className="node" ref={elementRef}>
         <div className="text">{label}</div>
       </div>
     </>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Node from './Components/Node';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Node label="A"/>
     <Node label="B"/>
+    <Node label="C"/>
   </React.StrictMode>
 );
