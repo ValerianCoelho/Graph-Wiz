fbc901b18ccc199f1a74805f69ac6addff30fedb
Valerian Coelho <valerian27012004@gmail.com>
2023-08-01 19:51:41 +0530

    Files Cleared

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <MenuBar/>
+      <ToolBar/>
+      <ViewPort/>
+      <OutputPannel/>
+      <TaskBar/>
+    </React.Fragment>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Node from './Components/Node';
-import Path from './Components/Path';
-import './style.css'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Node label="A" xPos="15" yPos="15"/>
-    <Node label="B" xPos="250" yPos="150"/>
-    <Node label="C" xPos="100" yPos="250"/>
-    <Path x1="15" y1="15" x2="250" y2="150"/>
-    <Path x1="100" y1="250" x2="250" y2="150"/>
-    <Path x1="100" y1="250" x2="15" y2="15"/>
+    <App/>
   </React.StrictMode>
 );
-
