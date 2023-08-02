63889589b9a97fc234c6eb671383623cc97d391f
Valerian Coelho <valerian27012004@gmail.com>
2023-08-02 18:59:44 +0530

    Global Styles added

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+const globalStyles = `
+  *{
+    margin: 0;
+    padding: 0;
+  }
+`
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <style>{globalStyles}</style>
     <App/>
   </React.StrictMode>
 );
