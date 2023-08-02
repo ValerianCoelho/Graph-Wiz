5a1ac4f123bb213557cd16e99bd5daf57263449e
Valerian Coelho <valerian27012004@gmail.com>
2023-08-02 20:06:12 +0530

    Styles Added

--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -1,14 +1,26 @@
 import React from "react";
+import theme from "../theme";
+import Logo from './Logo.svg'
 
 function MenuBar() {
+  const styles = {
+    body: {
+      display: 'flex',
+      gap: '2.4rem',
+      fontSize: '2rem',
+      backgroundColor: theme.background,
+      border: `.1rem solid ${theme.accent}`
+    }
+  }
+
   return (
-    <React.Fragment>
-      <img src="./Logo.svg"></img>
-      <div className="menu-title">File</div>
-      <div className="menu-title">Edit</div>
-      <div className="menu-title">View</div>
-      <div className="menu-title">Help</div>
-    </React.Fragment>
+    <div style={styles.body}>
+      <img src={Logo}></img>
+      <div style={styles.menu_title}>File</div>
+      <div style={styles.menu_title}>Edit</div>
+      <div style={styles.menu_title}>View</div>
+      <div style={styles.menu_title}>Help</div>
+    </div>
   )
 }
 
