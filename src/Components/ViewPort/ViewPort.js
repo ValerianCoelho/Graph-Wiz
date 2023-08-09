de3c0464532703a44ab12953c5d4df0f2f438c4d
Valerian Coelho <valerian27012004@gmail.com>
2023-08-09 07:37:00 +0530

    Panning Function Added

--- src/Components/ViewPort/ViewPort.js
+++ src/Components/ViewPort/ViewPort.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 import theme from "../theme";
+import interact from "interactjs";
 
 function ViewPort() {
   const [zoom, setZoom] = useState(1);
   const ViewportRef = useRef(null);
 
   const styles = {
-    body: {
+    wrapper: {
       backgroundColor: "#05050F",
       transform: `scale(${zoom})`, // Fix the transform property
       // transition: "transform 0.2s ease-in-out", // Add a smooth transition
       overflow: 'hidden'
     },
+    viewport: {
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'red'
+    }
   };
 
   useEffect(() => {
@@ -43,13 +49,31 @@ function ViewPort() {
         ViewportComponent.removeEventListener("mousewheel", handleMousewheel);
       }
     };
-  }, [zoom]); // Add `zoom` to the dependency array
+  }, [zoom]);
+
+  useEffect(()=> {
+    interact(ViewportRef.current).draggable({
+      listeners: {
+        move: (event)=> {
+          const target = event.target.children[0];
+          const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+          const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+          target.style.transform = `translate(${x}px, ${y}px)`;
+
+          target.setAttribute('data-x', x);
+          target.setAttribute('data-y', y);
+        }
+      }
+    })
+  }, []);
+
 
   return (
-    <div ref={ViewportRef} style={styles.body}>
-      <h1>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </h1>
+    <div ref={ViewportRef} style={styles.wrapper}>
+      <div style={styles.viewport}>
+
+      </div>
     </div>
   );
 }
