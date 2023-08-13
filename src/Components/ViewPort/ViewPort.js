2947fa3f36dac5e91ad47914108602cec443142d
Valerian Coelho <valerian27012004@gmail.com>
2023-08-13 21:15:09 +0530

    Fix Bug: Zoom Precision Increased

--- src/Components/ViewPort/ViewPort.js
+++ src/Components/ViewPort/ViewPort.js
@@ -20,7 +20,6 @@ function ViewPort() {
       height: '100%',
       backgroundColor: theme.background,
       transform: `scale(${zoom}) translate(${x}px, ${y}px)`,
-      // transition: "transform 1s ease"
     }
   };
 
@@ -32,8 +31,8 @@ function ViewPort() {
       const zoomChange = event.deltaY > 0 ? -zoomFactor : zoomFactor;
       const newZoom = zoom + zoomChange;
 
-      const minZoom = 0.1;
-      const maxZoom = 9;
+      const minZoom = 0.5;
+      const maxZoom = 2;
 
       const target = viewportRef.current;
 
@@ -43,20 +42,11 @@ function ViewPort() {
       const vx = viewportRef.current.getBoundingClientRect().left - viewportRef.current.offsetLeft;
       const vy = viewportRef.current.getBoundingClientRect().top - viewportRef.current.offsetTop;
 
-      const dx = -zoomChange * 10 * ((vx + (cx * newZoom)) - cx); // change these two equations
-      const dy = -zoomChange * 10 * ((vy + (cy * newZoom)) - cy); // change these two equations
-
-      // why vx negative when zoom out?
-      console.log("dx = zoomchange * (vx + (cx / newZoom)) - cx");
-      console.log(`${dx} = ${zoomChange} (${vx} + (${cx} * ${newZoom})) - ${cx}`);
-      console.log("dy = zoomchange (vy + (cy / newZoom)) - cy");
-      console.log(`${dy} = ${zoomChange} (${vy} + (${cy}*/ ${newZoom})) - ${cy}`);
+      const dx = - Math.ceil(zoomChange) * ((vx + (cx * newZoom)) - cx) / newZoom;
+      const dy = - Math.ceil(zoomChange) * ((vy + (cy * newZoom)) - cy) / newZoom;
 
       const x = (parseFloat(target.getAttribute('data-x')) || 0) + dx;
       const y = (parseFloat(target.getAttribute('data-y')) || 0) + dy;
-
-
-      //target.style.transform = `translate(${x}px, ${yy}px)`;
 
       target.setAttribute('data-x', x);
       target.setAttribute('data-y', y);
@@ -86,11 +76,10 @@ function ViewPort() {
     interact(ViewporWrappertRef.current).draggable({
       listeners: {
         move: (event)=> {
-          const target = event.target.children[0];
+          const target = event.target.children[0]; 
           const x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx / zoom;
           const y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy / zoom;
 
-          // target.style.transform = `scale(${zoom}) translate(${x}px, ${y}px)`;
           setX(x);
           setY(y);
 
@@ -104,23 +93,23 @@ function ViewPort() {
   return (
     <div ref={ViewporWrappertRef} style={styles.wrapper}>
       <div ref={viewportRef} style={styles.viewport}>
-      Now is the winter of our discontent
-Made glorious summer by this sun of York;
-And all the clouds that lour'd upon our house
-In the deep bosom of the ocean buried.
-Now are our brows bound with victorious wreaths;
-Our bruised arms hung up for monuments;
-Our stern alarums changed to merry meetings,
-Our dreadful marches to delightful measures.
-Grim-visaged war hath smooth'd his wrinkled front;
-And now, instead of mounting barded steeds
-To fright the souls of fearful adversaries,
-He capers nimbly in a lady's chamber
-To the lascivious pleasing of a lute.
-But I, that am not shaped for sportive tricks,
-Nor made to court an amorous looking-glass;
-I, that am rudely stamp'd, and want love's majesty
-To strut before a wanton ambling nymph;
+        Now is the winter of our discontent
+        Made glorious summer by this sun of York;
+        And all the clouds that lour'd upon our house
+        In the deep bosom of the ocean buried.
+        Now are our brows bound with victorious wreaths;
+        Our bruised arms hung up for monuments;
+        Our stern alarums changed to merry meetings,
+        Our dreadful marches to delightful measures.
+        Grim-visaged war hath smooth'd his wrinkled front;
+        And now, instead of mounting barded steeds
+        To fright the souls of fearful adversaries,
+        He capers nimbly in a lady's chamber
+        To the lascivious pleasing of a lute.
+        But I, that am not shaped for sportive tricks,
+        Nor made to court an amorous looking-glass;
+        I, that am rudely stamp'd, and want love's majesty
+        To strut before a wanton ambling nymph;
       </div>
     </div>
   );
