946de6c1e0158601ae267d0798bf64debf947f6b
Valerian Coelho <valerian27012004@gmail.com>
2023-10-05 17:37:26 +0530

    Shifted To Styled Component

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import store from "./Redux/store";
 import { Provider } from "react-redux";
+import { createGlobalStyle } from 'styled-components';
 
-const styles: string = `
+const GlobalStyles = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
   }
-`
+`;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <style> {styles} </style>
+    <GlobalStyles />
     <App />
   </Provider>
-)
+);
