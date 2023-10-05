02153ad53607ae6367653ce9c9fd388679c4663f
Valerian Coelho <valerian27012004@gmail.com>
2023-10-05 17:53:15 +0530

    Shifted To Styled Component

--- src/Components/Viewport/Viewport.tsx
+++ src/Components/Viewport/Viewport.tsx
@@ -7,6 +7,13 @@ import Theme from "../../Theme.tsx";
 import Node from "../../Graph Components/Node/Node.tsx";
 import Path from "../../Graph Components/Path/Path.tsx";
 import GraphPattern from '../Graph Pattern/GraphPattern.tsx';
+import styled from 'styled-components';
+
+const StyledViewportWrapper = styled.div`
+  background-color: ${Theme.viewportColor};
+  position:relative;
+`
+
 
 function Viewport(props: any) {
   const viewport = useRef<HTMLDivElement>(null);
@@ -49,16 +56,9 @@ function Viewport(props: any) {
     }
   }, []);
 
-  const styles: string = `
-    .viewport__body__wrapper {
-      background-color: ${Theme.viewportColor};
-      position:relative;
-    }
-  `;
-
   return (
     <>
-      <div className="viewport__body__wrapper">
+      <StyledViewportWrapper>
         <GraphPattern/>
         <div className="viewport__body" ref={viewport}>
           {props.node.map((nodeData: any, index: number) => (
@@ -66,8 +66,7 @@ function Viewport(props: any) {
           ))}
           <Path x1="10" y1="50" x2="500" y2="70"/>
         </div>
-      </div>
-      <style>{styles}</style>
+      </StyledViewportWrapper>
     </>
   );
 }
