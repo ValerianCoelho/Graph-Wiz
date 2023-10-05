95b0bef485e20b823d5a6ce1f33d88b9aa2852a6
Valerian Coelho <valerian27012004@gmail.com>
2023-10-05 17:30:25 +0530

    Shifted To Styled Component

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,27 @@ import Viewport from './Components/Viewport/Viewport.tsx'
 import Workspace from './Components/Workspace/Workspace.tsx'
 import TaskBar from './Components/Task Bar/TaskBar.tsx'
 import Theme from './Theme.tsx'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 4% auto 20%;
+  grid-template-rows: 5% auto 5%;
+  gap: 1px;
+  background-color: ${Theme.fgColor};
+  color: ${Theme.fgColor};
+`
 
 function App() {
-  const styles: string = `
-    .app-body {
-      height: 100vh;
-      display: grid;
-      grid-template-columns: 4% auto 20%;
-      grid-template-rows: 5% auto 5%;
-      gap: 1px;
-      background-color: ${Theme.fgColor};
-      color: ${Theme.fgColor};
-    }
-  `
   return (
-    <div className='app-body'>
-      <style> {styles} </style>
+    <StyledApp>
       <TitleBar/>
       <ToolBar/>
       <Viewport/>
       <Workspace/>
       <TaskBar/>
-    </div>
+    </StyledApp>
   )
 }
 
