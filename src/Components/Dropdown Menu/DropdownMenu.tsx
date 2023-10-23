1f949e2a3cdde7630a19c4262eb5cbea2c5e58d7
Ethan Menezes <emenezes4940@gmail.com>
2023-10-23 22:53:35 +0530

    changes to drop down file layout

--- src/Components/Dropdown Menu/DropdownMenu.tsx
+++ src/Components/Dropdown Menu/DropdownMenu.tsx
@@ -1,29 +1,29 @@
-import DropdownButton from "../../Dropdown Components/Dropdown Button/DropdownButton";
-import DropdownList from "../../Dropdown Components/Dropdown List/DropdownList";
-
 
  function DropdownMenu(props:any) {
 
     /* 
         Each drop down component needs a title and options prop
     */
-  
+
     
+
 
   return (
     <>
     <div className="drop-down" style={{width:`${props.width===undefined?"50px":props.width+"px"}`}}>
-        
-        <DropdownButton 
-          title={props.title}
-        />
 
-        <DropdownList 
-          options={props.options}
-          offset="30" 
-          width="100"
-          title={props.title}
-        />
+         <button className="drop-down__btn"  style={{width:"50px",height:"20px"}}>
+         {props.title}
+         </button>
+
+        <div className={`drop-down__list`} style={{width:`100px`,top:`30px`,display:`none`}}>
+        {props.options&&props.options.map((option:any)=>(
+             <div className="drop-down__list__item">
+             {option}
+             </div>
+         ))}
+
+     </div>
     </div>
     
     </>
