ba3b8b626e62ed958e831b79acaaf01b74b0d3e1
Valerian Coelho <valerian27012004@gmail.com>
2023-12-07 13:37:02 +0530

    Functionality [Titlebar]: Naigation Options Displayed

--- src/Widget Components/Navigation Dropdown/NavigationDropdown.tsx
+++ src/Widget Components/Navigation Dropdown/NavigationDropdown.tsx
@@ -7,30 +7,51 @@ const StyledNavigation = styled.ul`
   margin: 0 10px;
   font-family: 'Open Sans';
 
-  li {
-    padding: 2px 10px;
-    border-radius: 5px;
-  }
+  & > ul {
+    list-style-type: none;
 
-  li:hover {
-    background-color: #252545;
+    & > li {
+      padding: 2px 10px;
+      border-radius: 5px;
+      cursor: pointer;
+
+      &:hover {
+        background-color: #252545;
+      }
+    }
   }
+`;
+
+const StyledNavigationOptions = styled.ul`
+  position: absolute;
+  background-color: white;
+  z-index: 3;
 `
 
 function NavigationDropdown(props: any) {
   return (
     <>
       <StyledNavigation>
-      {
-        props.navigationData.map((Option: any)=> {
-          return (
-            <li>{Option.title}</li>
-          )
-        })
-      }
+        {props.navigationData.map((NavigationOption: any, NavigationOptionIndex: number) => (
+          <ul key={NavigationOptionIndex}>
+            <li>{NavigationOption.title}</li>
+            <StyledNavigationOptions>
+              {NavigationOption.children?.map((Section: any, SectionIndex: number) => (
+                <div key={NavigationOptionIndex * 10 + SectionIndex}>
+                  {Section.map((Option: any, OptionIndex: number) => (
+                    // Add the return statement here
+                    <li key={NavigationOptionIndex * 100 + SectionIndex * 10 + OptionIndex}>
+                      {Option.option}
+                    </li>
+                  ))}
+                </div>
+              ))}
+            </StyledNavigationOptions>
+          </ul>
+        ))}
       </StyledNavigation>
     </>
-  )
+  );
 }
 
 export default NavigationDropdown;
