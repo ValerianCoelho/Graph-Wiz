bccfad2e11b85e1b40278765fee31823e9cd05ef
Valerian Coelho <valerian27012004@gmail.com>
2023-12-07 13:07:01 +0530

    Navigation Data Added

---
+++ src/Components/Title Bar/NavigationData.tsx
@@ -0,0 +1,83 @@
+export const navigationData = [
+    {
+      "title": "File",
+      "children": [
+        [
+          {
+            "option": "Save",
+            "hotkey": "Ctrl + S"
+          },
+          {
+            "option": "Save As",
+            "hotkey": "Ctrl + Shift + S"
+          }
+        ],
+        [
+          {
+            "option": "New File",
+            "hotkey": "Ctrl + N"
+          },
+          {
+            "option": "New Window",
+            "hotkey": "Ctrl + Shift + N"
+          }
+        ],
+        [
+          {
+            "option": "Open File",
+            "hotkey": "Ctrl + O"
+          },
+          {
+            "option": "Open Template",
+            "hotkey": "Ctrl + O + T"
+          }
+        ],
+        [
+          {
+            "option": "Share",
+            "children": [
+              [
+                {
+                  "option": "Whatsapp",
+                },
+                {
+                  "option": "Instagram",
+                },
+                {
+                  "option": "Facebook",
+                },
+                {
+                  "option": "Discord",
+                }
+              ],
+              [
+                {
+                  "option": "Copy Link",
+                },
+                {
+                  "option": "Copy Github Link",
+                }
+              ]
+            ]
+          }
+        ],
+        [
+          {
+            "option": "Revert File",
+          },
+          {
+            "option": "Exit",
+          }
+        ]
+      ]
+    },
+    {
+      "title": "Edit",
+    },
+    {
+      "title": "View",
+    },
+    {
+      "title": "Help",
+    }
+  ]
