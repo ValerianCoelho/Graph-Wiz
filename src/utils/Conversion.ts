584fb1aa676f2d064c2101c363523a2b20b72337
Valerian Coelho <valerian27012004@gmail.com>
2024-02-22 12:02:52 +0530

    Conversion Function Added

---
+++ src/utils/Conversion.ts
@@ -0,0 +1,32 @@
+export function arabicToRoman(num: number) {
+    if (isNaN(num) || num < 1 || num > 3999) {
+        throw new Error("Invalid number. Please enter a number between 1 and 3999.");
+    }
+
+    const romanNumerals = [
+        { value: 1000, numeral: 'M' },
+        { value: 900, numeral: 'CM' },
+        { value: 500, numeral: 'D' },
+        { value: 400, numeral: 'CD' },
+        { value: 100, numeral: 'C' },
+        { value: 90, numeral: 'XC' },
+        { value: 50, numeral: 'L' },
+        { value: 40, numeral: 'XL' },
+        { value: 10, numeral: 'X' },
+        { value: 9, numeral: 'IX' },
+        { value: 5, numeral: 'V' },
+        { value: 4, numeral: 'IV' },
+        { value: 1, numeral: 'I' }
+    ];
+
+    let result = '';
+
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i].value) {
+            result += romanNumerals[i].numeral;
+            num -= romanNumerals[i].value;
+        }
+    }
+
+    return result;
+}
